e5239b4db53a79bd1c964066bd387eb7249c84eb
Naomy Morales <naomy.morales@upr.edu>
2021-11-04 19:39:55 -0400

    Working on auth error handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request, jsonify, make_response, abort
+from flask.helpers import flash
 from flask_cors import CORS,cross_origin
 from controller.accounts import BaseAccounts
 from model.Account import AccountDAO
@@ -12,7 +13,6 @@ import json
 import uuid
 import os
 
-# verify if the example works
 
 app = Flask(__name__)
 
@@ -25,13 +25,42 @@ CORS(app,supports_credentials=True,origins=['https://same-client-ui.herokuapp.co
 def index():
    return "Hello Buddies!!"
 
+#Custom error handlers
+@app.errorhandler(405)
+def no_valid_user(err):
+    app.logger.exception(err)
+    return "Username and password not valid, please try again",405
+#need to avoid overriding , some switch case of something
+@app.errorhandler(ValueError)
+def email_already_taken(err):
+    app.logger.exception(err)
+    return "Email is already taken, please try a different one",405
+
+def username_taken(err):
+    app.logger.exception(err)
+    return "Username is already taken, please try a different one",405
+
+def password_error(err):
+    app.logger.exception(err)
+    return "Password must contain atleast 8 characters, one uppercase letter, one lowercase letter, one number, and one special character",405
+
+#To get users or create them
 @app.route('/Same/accounts',methods=['GET','POST'])
 def users():
     if request.method=="GET":
         return BaseAccounts().getAllUsers()
     else:
-        return BaseAccounts().addUser(request.json)
-# need to work on this, for some reasson the put is not working
+        if  request.method=='POST':
+            res=BaseAccounts().addUser(request.json)
+            if res=="this email is already taken":
+                return email_already_taken("error")
+            elif res=="username is already taken, please try a different one":
+                return username_taken("error")
+            elif res=="password must contain atleast 8 characters, atleast one uppercase letter, atleast one lowercase letter, atleast one number, and atleast one special character":
+                return password_error("error")
+            return res
+
+#getting a user
 @app.route('/Same/accounts/<uid>',methods=['GET', 'PUT', 'DELETE'])
 def getSpecificUser(uid):
     if request.method == "GET":
@@ -43,13 +72,12 @@ def getSpecificUser(uid):
     else:
         return jsonify('Method not allowed'), 405
 
-# account login route
 
+# account login route
 @app.route('/Same/login', methods=["POST"])
 def login():
     if request.method=="POST":
         res = BaseAccounts().Log_in(request.json)
-        
         if res:
             uid = res.get('uid')
             user_name = res.get('user_name')
@@ -59,14 +87,15 @@ def login():
             token = jwt.encode({'user': uid}, app.config['SECRET_KEY'])
             cookie = make_response(res)
             cookie.set_cookie('access_token',token)
-           
 
             return cookie
 
-    return jsonify('Username and password not valid, please try again'), 405   
-
-#will be changed   
-
+    return no_valid_user("error")
+        
+    # return abort(405, description="Username and password not valid, please try again")
+    # return default_error("Username invalid")
+ 
+#will be changed
 @app.route('/Same/accounts/logout/<uid>',methods=["POST"])
 def logout(uid):
     token=request.cookies.get('access_token')
@@ -87,15 +116,12 @@ def logout(uid):
 @app.route('/Same/accounts/getCookieOwner',methods=["GET"])
 def getCookie():
     cookie = request.cookies.get('access_token')
-   
 
     if(cookie is None):
         return '',403
 
-
     decode_token = jwt.decode(cookie,app.config['SECRET_KEY'],algorithms=["HS256"])
     user = decode_token.get('user')
-
 
     account= BaseAccounts().getCookieOwner(user)
 
